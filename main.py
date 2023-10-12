dfd5e4b7a7eb464ebc8a527c53ef9b6be44eb3fa
Sanjin <102841251+duckduckgrayduck@users.noreply.github.com>
2023-10-12 15:37:58 -0500

    Check that at least one document was selected

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from documentcloud.addon import SoftTimeOutAddOn
 class ChangeVisibility(SoftTimeOutAddOn):
     """Add-On that changes access level for large set of documents"""
     def main(self):
+        if not self.documents:
+            self.set_message("Please select at least one document.")
+            return
         for document in self.get_documents():
             if document.edit_access:
                 document.access = self.data.get('access')
